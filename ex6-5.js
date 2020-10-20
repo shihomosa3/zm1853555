e8bed02be4a4fe061dbb193385b0aecc067c1f0c
shihomosa3 <zm18535@josai.ac.jp>
2020-10-20 02:02:52 +0000

    10/20-2

--- ex6-5.js
+++ ex6-5.js
@@ -1,5 +1,7 @@
-var who ='world';
-function sayhello(){alert('Hello,'+ who +'!');}
-function taro(){who ='太郎';}
+function sayhello(){var who ='world';
+alert('Hello,'+ who +'!');}
+function taro() {who ='太郎';}
 function hanako(){who ='花子';}
+
+
 
