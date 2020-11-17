b007cf76a7e942b5cbf71718d7b4ecc0e74b0664
shihomosa3 <zm18535@josai.ac.jp>
2020-11-17 02:09:22 +0000

    11-17

--- ex06-6.js
+++ ex06-6.js
@@ -1,4 +1,12 @@
-var who66 ='world';function sayhello66() {alert('Hello,' +who66 + '!');}
-function taro66() {who ='太郎';}
-function hanako66() {who ='花子';}
-function gakita66() {alert(who='が来た');}
+var who66 = 'world';
+function sayhello66() {
+alert('Hello, ' + who66 + '!');
+} 
+function taro() {
+who66 = '太郎' ;
+} 
+function hanako() {
+who66 = '花子';
+}
+function gakita() {alert(who66 + 'が来た');
+}
