19acad8ea2f3e9c893ef6245ac82055d3d8c658a
shihomosa3 <zm18535@josai.ac.jp>
2020-12-01 02:02:43 +0000

    12/1

--- ex06-9.js
+++ ex06-9.js
@@ -4,3 +4,12 @@ document.getElementById('txt1').innerHTML = 'ナンバーワン！';
 function imgchange1() {
 document.getElementById('logo').src = './image/bulbon.gif';
 }
+function show_innerHTMLtxt1(){
+var result1 = document.getElementById('txt1').innerHTML;
+alert(result1);
+}
+
+function show_innerHTML(){
+var result = document.getElementById('logo').innerHTML;
+ alert(result);
+}
